b03c0498dd0d91ba89ddf76d98399cba217c7e77
Alaskarli <nazrinna@code.edu.az>
2023-01-22 20:40:16 -0800

    Homework

---
+++ Homework/homeworks/Program.cs
@@ -0,0 +1,82 @@
+﻿
+
+
+
+//int result = SumOfNumber(5);
+//Console.WriteLine(result);
+
+//int result = DevideByNumber(39);
+//Console.WriteLine(result);
+
+//static int DevideByNumber(int number)
+//{
+//	int result = number;
+//	if (number % 7 == 0)
+//	{
+//		Console.WriteLine("bolunur");
+//	}
+//	else
+//	{
+//		int qaliq = number % 7;
+//		if (qaliq > 3)
+//		{
+//			while (result % 7 != 0)
+//			{
+//				result++;
+//			}
+
+//		}
+//		else
+//		{
+//			while (result % 7 != 0)
+//			{
+//				result--;
+//			}
+//		}
+//	}
+//	return result;
+
+
+//static int SumOfNumber(int number)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < number; i++)
+//    {
+//        sum += i;
+//    }
+//    return sum;
+//}
+
+
+//AddStringNumbers("Nazrin", "Alaskali");
+
+
+//static void AddStringNumbers(string word1,string word2)	
+//{
+//	int count1 = 0;
+//	int count2 = 0;
+//	foreach (var word1item in word1)
+//	{
+//		count1++;
+//	}
+//	foreach (var word2item in word2)
+//	{
+//		count2++;
+
+//	}
+//	if (count1 == count2)
+//	{
+//		Console.WriteLine("uzunluqlari beraberdir");
+//	}
+//	else if (count1>count2)
+//	{
+//		Console.WriteLine("1ci soz 2ci sozden uzundur");
+//	}
+//	else
+//	{
+//		Console.WriteLine("2ci soz 1ci sozden uzundur");
+//	}
+//}
+
+
+
